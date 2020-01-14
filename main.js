93407d2d5c920dd95ba3bc22eb274110e0d5d699
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2020-01-13 17:33:20 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 var playerTurn = 0;
+var playerColor0 = "orange";
+var playerColor1 = "orange";
+
+var turnBox1 = "";
 
 function clickHandler(pieceName) {
+  if(playerTurn == 0){
+     if(document.getElementById(pieceName).style.backgroundColor == playerColor0){
+        
+     }
+  }
+  
   //Background Color
   alert(document.getElementById(pieceName).style.backgroundColor);
 }
