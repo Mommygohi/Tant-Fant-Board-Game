920d71840160e89bb32e6b5a9e2513900f3adff1
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2020-01-14 08:46:41 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ var playerTurn = 0;
 var playerColor0 = "orange";
 var playerColor1 = "orange";
 
-var turnBox1 = "";
+var turnBox1 = "none";
 
 function clickHandler(pieceName) {
   if(playerTurn == 0){
-     if(document.getElementById(pieceName).style.backgroundColor == playerColor0){
-        
+     if(turnBox1 == "none" && document.getElementById(pieceName).style.backgroundColor == playerColor0){
+        turnBox1 = pieceName;
+     } else if(turnBox1 != "none" && document.getElementById(pieceName).style.backgroundColor != playerColor1){
+        var turnBox2 = pieceName;
+        document.getElementById(turnBox1).style.backgroundColor = "white";
+        document.getElementById(turnBox2).style.backgroundColor = playerColor0;
+       //Reset
+       turnBox1 = "none";
+       playerTurn++;
+       alert("Player Turn:" + playerTurn);
+       alert("Turn Box 1: " + turnBox1);
      }
   }
-  
-  //Background Color
-  alert(document.getElementById(pieceName).style.backgroundColor);
 }
